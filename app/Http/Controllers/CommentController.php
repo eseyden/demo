62ca91295267913b8ef82cc128b08441ace8561f
Eric Seyden <ericseyden@yahoo.com>
2022-03-21 14:05:57 -0600

    comment queries

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,8 +10,16 @@ class CommentController extends Controller
     function index()
     {   
         $data = [
-            'candyComments' => Comment::where('comments','LIKE', '%candy%')->get()
+            'candyComments' => Comment::where('comments','LIKE', '%candy%')->get(),
+            'callComments' => Comment::where('comments','LIKE', '%call%')->get(),
+            'referComments' => Comment::where('comments','LIKE', '%refer%')->get(),
+            'signatureComments' => Comment::where('comments','LIKE', '%sig%')->get(),
         ];
+        $data['miscComments'] = Comment::where('comments','NOT LIKE', '%candy%')
+            ->where('comments','NOT LIKE', '%call%')
+            ->where('comments','NOT LIKE', '%refer%')
+            ->where('comments','NOT LIKE', '%sig%')
+            ->get();
         return view('index',$data);
     }
 }
